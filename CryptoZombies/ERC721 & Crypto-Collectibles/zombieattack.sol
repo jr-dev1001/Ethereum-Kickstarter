475bcdb6f5b6b581026f580e34fcac4f4253c2a8
jr-dev1001 <vamsisurakarapu@gmail.com>
2025-04-02 20:11:51 +0530

    ERC721 implementation

--- CryptoZombies/ERC721 & Crypto-Collectibles/zombieattack.sol
+++ CryptoZombies/ERC721 & Crypto-Collectibles/zombieattack.sol
@@ -8,7 +8,7 @@ contract ZombieAttack is ZombieHelper {
   // Create attackVictoryProbability here
   uint attackVictoryProbability = 70;
   function randMod(uint _modulus) internal returns(uint) {
-    randNonce++;
+    randNonce = randNonce.add(1);
     //now keyword is deprecated so we use block.timestamp here as alternative
     return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % _modulus;
   }
@@ -18,13 +18,13 @@ contract ZombieAttack is ZombieHelper {
     Zombie storage enemyZombie = zombies[_targetId];
     uint rand = randMod(100);
     if(rand <= attackVictoryProbability){
-      myZombie.winCount++;
-      myZombie.level++;
-      enemyZombie.lossCount++;
+      myZombie.winCount = myZombie.winCount.add(1);
+      myZombie.level = myZombie.level.add(1);
+      enemyZombie.lossCount = enemyZombie.lossCount.add(1);
       feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
     } else {  
-      myZombie.lossCount++;
-      enemyZombie.winCount++;
+      myZombie.lossCount = myZombie.lossCount.add(1);
+      enemyZombie.winCount = enemyZombie.winCount.add(1);
       _triggerCooldown(myZombie);
     }
   }
--- CryptoZombies/ERC721 & Crypto-Collectibles/zombieownership.sol
+++ CryptoZombies/ERC721 & Crypto-Collectibles/zombieownership.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 import "./zombieattack.sol";
 import "./erc721.sol";
 
+/// @title A contract that manages transferring zombie ownership
+/// @author jr-dev1001
+/// @dev Complaint with Openzeppelin's implementation of ERC721 spec draft
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
   function balanceOf(address _owner) external override view returns (uint256) {
@@ -11,15 +14,23 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
   }
 
   function ownerOf(uint256 _tokenId) external view returns (address) {
-    // 2. Return the owner of `_tokenId` here
     return zombieToOwner(_tokenId);
   }
 
-  function transferFrom(address _from, address _to, uint256 _tokenId) external override payable {
-
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to]++;
+    ownerZombieCount[_from]--;
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
   }
 
-  function approve(address _approved, uint256 _tokenId) external override payable {
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+    _transfer(_from, _to, _tokenId);
+  }
 
+  function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
+    zombieApprovals[_tokenId] = _approved;
+    emit Approval(msg.sender, _approved, _tokenId);
   }
 }
