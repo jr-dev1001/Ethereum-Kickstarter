9b0b1d4a0d3fcabd485339582bf52484b06ebd44
Vamsi Surakarapu <85192850+jr-dev1001@users.noreply.github.com>
2025-08-01 15:32:45 +0530

    detail explanation on 13.gatekeeperOne.sol

--- Ethernaut CTF/13.gatekeeperOne.sol
+++ Ethernaut CTF/13.gatekeeperOne.sol
@@ -26,7 +26,55 @@ contract GatekeeperOne {
         return true;
     }
 }
+// Gate One
+// Automatically passed, because msg.sender = 'Attack' but tx.origin = 'player' address
 
+//Gate Two
+// Each OPCODE => GAS
+// gasleft() => amount of gas left at this point
+// total gas = (8191*k) + i
+
+/*Gate Three*/
+// bytes8 _gateKey => 0x B1 B2 B3 B4 B5 B6 B7 B8
+//Requirement #1
+//uint32(uint54(_gateKey))== uint16(uint64(_gateKey))
+//uint64(_gateKey) => numerical representation of _gateKey
+
+//uint32(uint64(_gateKey)) => 0x B5 B6 B7 B8
+//uint16(uint64(_gateKey)) => 0x B7 B8
+// 0x B5 B6 B7 B8 = 0x 00 00 B7 B8
+//To clear requirement one, B5 and B6 must be zero
+
+//Request #2
+//uint32(uint64(_gateKey)) != uint64(_gateKey)
+//uint32(uint64(_gateKey)) => 0x B5 B6 B7 B8
+// 0x 00 00 00 00 B5 B6 B7 B8 != 0x B1 B2 B3 B4 B5 B6 B7 B8
+
+//Request #3
+// uint32(uint64(_gateKey)) == uint16(uint160(tx.origin))
+//LHS => 0x B5 B6 B7 B8
+//uint160 = 20 bytes = length of an address on ethereum
+//uint160(tx.origin) = numerical representation of the address
+//uint16(uint160(tx.origin)) => last two bytes of tx.origin
+//0x B5 B6 B7 B8 == last two bytes of tx.origin
+//0x B7 B8 == last two bytes of tx.origin
+
+//Req #1: B5 and B6 must be zeroes
+//Req2 #2: B1 B2 B3 B4 must NOT all be zeroes || B1 B2 B3 B4 equal to first 4 bytes of tx.origin
+//Req #3: B7 B8 is equal to last 2 bytes of tx.origin
+
+//BitMasking
+//Bitwise AND operator:
+// 1 & 0 = 0, 0 & 0 = 0, 1 & 1 = 1
+//
+// creating key => 
+/*
+bytes8 gatekey = bytes8(uint64(tx.origin) & 0xFFFFFFFF0000FFFF); //gate3 satisfies
+for (uint256 i =0; i<300; i++){
+    uint256 totalGas = i+(8191*3);
+    gatekeeper.enter{gas : totalGas}(gateKey);
+}
+*/
 
 contract GatekeeperOneAttack {
     function attack(address gatekeeper) public {
